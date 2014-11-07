dc581baa676eeb81332d8e0fa7d7f0c7a5a76c38
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-07 21:48:12 +0900

    Add Strings Step

---
+++ Strings.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// mutable string
+var language = "Swift"
+
+// immutable string
+let immutableLanguage = "Swift"
+
+// Concatenation
+var greeting = "Hello, " + language
+
+// String Interpolation
+greeting = "Hello, \(language)"
+
+// one line
+println("List: 1 - Apple 2 - Bananas 6 - Oranges")
+
+// \n add new line
+println("List:\n 1 - Apple 2 - Bananas 6 - Oranges")
+
+// \t add new tab
+println("List:\n1\tApple\n2\tBananas\n6\tOranges")
+
+// Swift supports special character like emoji
+// any unicode characters are allowed
+println("List:\n1\t🍎\n2\t🍌\n6\t🍅")
