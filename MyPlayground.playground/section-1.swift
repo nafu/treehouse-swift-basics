b92f532e0eac6203797f84f51232116a14467bda
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-07 20:57:05 +0900

    Add Variables Step

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -3,3 +3,24 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// syntax error
+// var str = "Hello, playground
+
+// syntax error
+// var str = Hello, playground
+
+// access variable str
+str
+
+// you can change the variable
+str = "Hello, World"
+
+// define another variable
+var modernProgrammingLanguage = "Swift"
+
+// join string
+var greeting = "Hello, " + modernProgrammingLanguage
+
+str = "Hello, "
+greeting = str + modernProgrammingLanguage
