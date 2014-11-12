e2d5c9ec452429f4dd660c6a6cbc65697c7e0db9
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-12 14:36:45 +0900

    Add Numbers Step

---
+++ Numbers.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let language = "Swift"
+
+let year: Int = 2014 // Int or Integer
+
+var version: Double = 1.0 // Double = 15 decimal and Flout = 7 decimal
+
+var isAwesome = true // Boolean or Bool
