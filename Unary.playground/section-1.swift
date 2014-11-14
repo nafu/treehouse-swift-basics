01235cb77e64bc9a578c44ae745540b7628f9815
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-14 21:43:11 +0900

    Add Unary Operators Step

---
+++ Unary.playground/section-1.swift
@@ -0,0 +1,36 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var levelScore = 0
+
+levelScore = levelScore + 1
+levelScore++
+levelScore
+
+var totalScore = 0
+totalScore = levelScore++
+totalScore
+levelScore
+
+totalScore = ++levelScore
+totalScore
+levelScore
+
+levelScore--
+levelScore
+
+--levelScore
+levelScore
+
+// ++ increment unary operator
+// -- decrement unary operator
+// - negative operator
+
+levelScore = levelScore * -1
+levelScore = -levelScore
+
+// ! NOT Operator
+
+let on = false
+let off = !on
