72c005194b46d26b4a36f5382b43bd4ecf560daa
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-15 23:53:19 +0900

    Add What is an Array? Step

---
+++ Arrays.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var a = []
+
+var strings: [String] = ["Return Calls", "Write Blogpost", "Cook Dinner"]
+
+var todo = ["Return Calls", "Write Blogpost", "Cook Dinner"]
+todo += ["Pickup Laundry", "Buy Bulbs"]
+
+todo[0]
+
+todo.count
