7e304fb369c049a927c1ea3a398314fb0f57430e
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-16 20:33:28 +0900

    Add What is a Dictionary? Step

---
+++ Dictionary.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// Code     Country Name
+// or
+// Key      Value
+// CA       Canada
+// BE       Belgium
+// FR       France
+// DE       Germany
+// UK       United Kingdom
+// US       United States
+
+var countries = ["CA": "Canada",
+                 "BE": "Belgium",
+                 "FR": "France"]
+
+countries["CA"]
+
+countries["US"] = "United States"
+countries
+
+countries["US"] = "United States of America"
+
+let item = countries.removeValueForKey("CA")
+item
+countries
