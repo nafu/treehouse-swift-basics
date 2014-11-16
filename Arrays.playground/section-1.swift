fa7e4f7038f4af47ded047dae6ae04a03b64dd81
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-16 19:22:45 +0900

    Add Modifying an Array Step

--- Arrays.playground/section-1.swift
+++ Arrays.playground/section-1.swift
@@ -12,3 +12,19 @@ todo += ["Pickup Laundry", "Buy Bulbs"]
 todo[0]
 
 todo.count
+
+todo.append("Edit Blog Post")
+todo
+
+todo[2] = "Clean Dishes"
+todo
+
+let item = todo.removeLast()
+item
+todo
+
+let item2 = todo.removeAtIndex(1)
+item2
+todo
+
+todo.insert("Call Mom", atIndex: 0)
